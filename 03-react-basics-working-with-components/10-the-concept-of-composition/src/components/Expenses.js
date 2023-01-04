43cf6a606ab071ed48a4f86aa6c7ccacd020ed51
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-03 21:15:22 -0600

    finished 03-react-basics-working-with-components

---
+++ 03-react-basics-working-with-components/10-the-concept-of-composition/src/components/Expenses.js
@@ -0,0 +1,34 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const items = props.items;
+  return (
+    <Card className="expenses">
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      />
+      <ExpenseItem
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
