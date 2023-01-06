a1e19bc18631aecd03e72689bb99698b5715f06c
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-06 17:28:45 -0600

    06-styling

--- 06-styling/07-using-css-modules/src/components/UI/Button/Button.js
+++ 06-styling/07-using-css-modules/src/components/UI/Button/Button.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import styles from "./Button.module.css";
+/*
 import styled from "styled-components";
-//import './Button.css';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,6 +13,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -23,14 +29,18 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+*/
 
-/*
-const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button
+      type={props.type}
+      className="{styles.button}"
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
 };
-*/
+
 export default Button;
