1a384f50e79188c85ef3eafb669a8407eb12d1cd
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-09 15:12:48 -0600

    11-practice-food-order-app

---
+++ 11-practice-food-order-app/04-adding-individual-meal-items/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,11 @@
+import css from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={CSS.form}>
+      <div></div>
+    </form>
+  );
+};
+
+export default MealItemForm;
