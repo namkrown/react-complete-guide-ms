2addb67e4a961e16d1d22954c45fd0aedc8bbdcb
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-10 13:56:19 -0600

    11-practice-food-order-app/08-managing-cart-and-modal-state

--- 11-practice-food-order-app/08-managing-cart-and-modal-state/src/App.js
+++ 11-practice-food-order-app/08-managing-cart-and-modal-state/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- 11-practice-food-order-app/08-managing-cart-and-modal-state/src/components/Layout/HeaderCartButton.js
+++ 11-practice-food-order-app/08-managing-cart-and-modal-state/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import css from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartContext = useContext(CartContext);
+
+  const sumCallbackFx = (accumulator, item) => {
+    return accumulator + item.amount;
+  };
+  const numberOfCartItems = cartContext.items.reduce(sumCallbackFx, 0);
   return (
     <button className={css.button} onClick={props.onClick}>
       <span className={css.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={css.badge}>3</span>
+      <span className={css.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
