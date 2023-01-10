72d103ac54a660ec43c9c864162f08770570f377
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-10 14:50:07 -0600

    11-practice-food-order-app/09-adding-a-cart-reducer

--- 11-practice-food-order-app/09-adding-a-cart-reducer/src/components/Meals/MealItem/MealItem.js
+++ 11-practice-food-order-app/09-adding-a-cart-reducer/src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+
+import CartContext from "../../../store/cart-context";
 import MealItemForm from "./MealItemForm";
 import css from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const cartContext = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    console.log("MealItem::addToCartHandler-start");
+    const item = {
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    };
+    cartContext.addItem(item);
+    console.log("MealItem::addToCartHandler-end");
+  };
+
   return (
     <li className={css.meal}>
       <div>
@@ -11,7 +28,7 @@ const MealItem = (props) => {
         <div className={css.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- 11-practice-food-order-app/09-adding-a-cart-reducer/src/components/Meals/MealItem/MealItemForm.js
+++ 11-practice-food-order-app/09-adding-a-cart-reducer/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,38 @@
+import { useRef, useState } from "react";
+
 import Input from "../../UI/Input";
 import css from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    console.log("submitHandler-start");
+    event.preventDefault();
+
+    const enteredAmountTxt = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmountTxt;
+
+    if (
+      enteredAmountTxt.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      console.log("MealItemForm::submitHandler setAmountIsValid(false)");
+      setAmountIsValid(false);
+    } else {
+      console.log("MealItemForm::submitHandler setAmountIsValid(true)");
+      setAmountIsValid(true);
+      props.onAddToCart(enteredAmountNumber);
+    }
+    console.log("submitHandler-end");
+  };
+
   return (
-    <form className={css.form}>
+    <form className={css.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +44,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {amountIsValid && <p>Please a valid amount (1-5)</p>}
     </form>
   );
 };
