6812d81af7d01be29b1df5a3034245122a695e43
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-10 20:34:16 -0600

    12-a-look-behind-the-scenes/05-preventing-function-recreation-usecallback start

---
+++ 12-a-look-behind-the-scenes/05-preventing-function-recreation-usecallback/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./Demo/DemoOutput";
+
+function App() {
+  const [, setShowParagraph] = useState(false);
+  console.log("APP RUNNING");
+  const btnOnClickHandler = (event) => {
+    setShowParagraph((previousShowParagraph) => {
+      setShowParagraph(!previousShowParagraph);
+    });
+  };
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <Button onClick={btnOnClickHandler}>Toggle Paragraph</Button>
+      <DemoOutput show={false} />
+    </div>
+  );
+}
+
+export default App;
