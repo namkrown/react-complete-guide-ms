cbec9b1cbf0542b54ef0617a35789ef07909c0b6
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-10 18:37:50 -0600

    11-practice-food-order-app/12-finished

--- 11-practice-food-order-app/12-finished/src/components/Layout/HeaderCartButton.js
+++ 11-practice-food-order-app/12-finished/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import css from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  console.log("HeaderCartButton::start");
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
-
+  const { items } = cartContext;
+  console.log(items);
   const sumCallbackFx = (accumulator, item) => {
     return accumulator + item.amount;
   };
-  const numberOfCartItems = cartContext.items.reduce(sumCallbackFx, 0);
+  const numberOfCartItems = items.reduce(sumCallbackFx, 0);
+
+  const buttonCssClasses = `${css.button} ${btnIsHighlighted ? css.bump : ""}`;
+  //const buttonCssClasses = `${css.button} ${css.bump}`;
+
+  console.log(buttonCssClasses);
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setBtnIsHighlighted(true);
+
+      const timerId = setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+
+      return () => {
+        clearTimeout(timerId);
+      };
+    }
+  }, [items]);
+
+  console.log("HeaderCartButton::end");
+
   return (
-    <button className={css.button} onClick={props.onClick}>
+    <button className={buttonCssClasses} onClick={props.onClick}>
       <span className={css.icon}>
         <CartIcon />
       </span>
