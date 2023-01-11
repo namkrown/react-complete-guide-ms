81f3a4f8c4c44fe2caf96c63a0ba9affb1f9e840
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-11 15:13:19 -0600

    16-working-with-forms/02-dealing-with-form-submission-getting-values

---
+++ 16-working-with-forms/02-dealing-with-form-submission-getting-values/src/components/SimpleInput.js
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log("enteredName=" + enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log("enteredValue=" + enteredValue);
+
+    // Not ideal - directly updating DOM, instead of virtual DOM and letting react do it
+    //nameInputRef.current.value = "";
+    setEnteredName("");
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+/*
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log("enteredName=" + enteredName);
+  };
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+*/
+
+export default SimpleInput;
