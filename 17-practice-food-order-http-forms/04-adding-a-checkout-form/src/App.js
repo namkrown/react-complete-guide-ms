d1da89eb60b933bd9b0721feaef1ed2f130fe258
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-12 01:43:30 -0600

    17-practice-food-order-http-forms/04-adding-a-checkout-form

--- 17-practice-food-order-http-forms/04-adding-a-checkout-form/src/App.js
+++ 17-practice-food-order-http-forms/04-adding-a-checkout-form/src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -13,7 +13,9 @@ function App() {
   };
 
   const hideCartHandler = () => {
+    console.log("hideCartHandler-start");
     setCartIsShown(false);
+    console.log("hideCartHandler-end");
   };
 
   return (
