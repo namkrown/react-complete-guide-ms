ba11aa8a61c08ebc7a0303de6bf176ca00be1d84
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-17 17:51:24 -0600

    20-building-mpas-with-react-router-v6/04-adding-links

--- 20-building-mpas-with-react-router-v6/04-adding-links/src/pages/Home.js
+++ 20-building-mpas-with-react-router-v6/04-adding-links/src/pages/Home.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <div>
       <h1>Home</h1>
+      <p>
+        Default html anchor<a href="/products">the list of products.</a>
+      </p>
+      <p>
+        Got to <Link to="/products">the list of products.</Link>
+      </p>
     </div>
   );
 }
