18a772f0529e046830db878718e0524b607789b6
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-18 16:37:14 -0600

    20-building-mpas-with-react-router-v6/08-navigating-programmatically

--- 20-building-mpas-with-react-router-v6/08-navigating-programmatically/src/pages/Home.js
+++ 20-building-mpas-with-react-router-v6/08-navigating-programmatically/src/pages/Home.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    // Example of how you can programatically navigate
+    navigate("/products");
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <p>
         Got to <Link to="/products">the list of products.</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </div>
   );
