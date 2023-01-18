c40a206208b74b4d6c2347ae0e738a236fc96069
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-18 15:53:14 -0600

    20-building-mpas-with-react-router-v6/06-error-route-404

---
+++ 20-building-mpas-with-react-router-v6/06-error-route-404/src/pages/Error.js
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import css from "./Error.module.css";
+
+function ErrorPage() {
+  return (
+    <>
+      <MainNavigation />
+      <main className={css.content}>
+        <h1>An error occurred!</h1>
+        <p>Could not find this page!</p>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
