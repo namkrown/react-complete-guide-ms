b2ce4addf4d83c33a594b0de40cc031a9a561034
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-19 20:25:34 -0600

    20-building-mpas-with-react-router-v6/14-challenge-solution/backend

--- 20-building-mpas-with-react-router-v6/14-challenge-solution/frontend/src/App.js
+++ 20-building-mpas-with-react-router-v6/14-challenge-solution/frontend/src/App.js
@@ -20,8 +20,38 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/RootLayout";
+import EventsRootLayout from "./pages/EventsRootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
