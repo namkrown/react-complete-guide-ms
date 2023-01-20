dc2a8d2280d6fe76fc94bb1bb48868209ae5c007
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-20 11:58:35 -0600

    20-building-mpas-with-react-router-v6/18-user-feedback-usenavigation

--- 20-building-mpas-with-react-router-v6/18-user-feedback-usenavigation/backend/routes/events.js
+++ 20-building-mpas-with-react-router-v6/18-user-feedback-usenavigation/backend/routes/events.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res, next) => {
     const events = await getAll();
     setTimeout(() => {
       res.json({ events: events });
-    }, 1500);
+    }, 3000);
   } catch (error) {
     next(error);
   }
