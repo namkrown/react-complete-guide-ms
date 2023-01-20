f6276c116a27974dddd4840c866f17433e7a5011
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-20 12:05:25 -0600

    20-building-mpas-with-react-router-v6/19-returning-responses

---
+++ 20-building-mpas-with-react-router-v6/19-returning-responses/frontend/src/pages/EventsPage.js
@@ -0,0 +1,57 @@
+import { useLoaderData } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
+export default function EventsPage() {
+  const events = useLoaderData();
+  return <EventsList events={events} />;
+}
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //setError("Fetching events failed.");
+  } else {
+    const responseData = await response.json();
+    return responseData.events;
+  }
+}
+// Code required without leveraging react-router loader
+/* 
+import { useEffect, useState } from "react";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {  
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+    return (
+    <>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
+}
+*/
