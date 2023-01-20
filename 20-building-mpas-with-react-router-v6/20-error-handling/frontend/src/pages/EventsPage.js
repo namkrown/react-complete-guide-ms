95d5164360c88be923a43e19cfaa1d72f16447d8
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-20 16:55:26 -0600

    20-building-mpas-with-react-router-v6/20-error-handling

--- 20-building-mpas-with-react-router-v6/20-error-handling/frontend/src/pages/EventsPage.js
+++ 20-building-mpas-with-react-router-v6/20-error-handling/frontend/src/pages/EventsPage.js
@@ -4,57 +4,46 @@ import EventsList from "../components/EventsList";
 
 export default function EventsPage() {
   const responseData = useLoaderData();
-  //const events = useLoaderData();
-  const events = responseData.events;
-  return <EventsList events={events} />;
+
+  if (responseData.isError) {
+    return <p>{responseData.message}</p>;
+  } else {
+    const events = responseData.events;
+    return <EventsList events={events} />;
+  }
+}
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events1");
+
+  if (!response.ok) {
+    //throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  }
+  return response;
+}
+
+/*
+export default function EventsPage() {
+  const responseData = useLoaderData();
+
+  if (responseData.isError) {
+    return <p>{responseData.message}</p>;
+  } else {
+    const events = responseData.events;
+    return <EventsList events={events} />;
+  }
 }
 
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //setError("Fetching events failed.");
+    return { isError: true, message: "Could not fetch Events" };
   } else {
-    //const responseData = await response.json();
-    //return responseData.events;
     return response;
   }
-}
-// Code required without leveraging react-router loader
-/* 
-import { useEffect, useState } from "react";
-
-import EventsList from "../components/EventsList";
-
-function EventsPage() {  
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/events");
-
-      if (!response.ok) {
-        setError("Fetching events failed.");
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-    return (
-    <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    </>
-  );
 }
 */
