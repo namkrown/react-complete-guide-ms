cd311c03fb404fc46b46c7a5313d802f3d4aea11
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-20 10:21:37 -0600

    20-building-mpas-with-react-router-v6/15-data-fetching-example

--- 20-building-mpas-with-react-router-v6/15-data-fetching-example/frontend/src/pages/EventsPage.js
+++ 20-building-mpas-with-react-router-v6/15-data-fetching-example/frontend/src/pages/EventsPage.js
@@ -1,27 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const events = [
-  {
-    id: "e1",
-    title: "Some Event",
-  },
-  {
-    id: "e2",
-    title: "Another Event",
-  },
-];
+import EventsList from "../components/EventsList";
 
-export default function EventsPage() {
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>Events</h1>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 }
+
+export default EventsPage;
