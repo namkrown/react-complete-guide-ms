d5a4c195613c8441b58b6f2da4a16d53c829b8de
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-23 17:50:39 -0600

    22-authentication-updated/06-logout

---
+++ 22-authentication-updated/06-logout/frontend/src/pages/Authentication.js
@@ -0,0 +1,49 @@
+import AuthForm from "../components/AuthForm";
+import { json, redirect } from "react-router-dom";
+
+import { setAuthToken } from "../util/auth";
+
+export default function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw new json({ messge: "Unsupported Mode." }, { status: 422 });
+  }
+
+  const formData = await request.formData();
+
+  const authData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const url = "http://localhost:8080/" + mode;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const fetchConfig = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(authData),
+  };
+
+  const response = await fetch(url, fetchConfig);
+
+  if (response.status === 401 || response.status === 422) {
+    return response;
+  } else if (!response.ok) {
+    throw new json({ messge: "Could not authenticate user" }, { status: 500 });
+  }
+
+  const responseData = await response.json();
+  const authToken = responseData.token;
+
+  setAuthToken(authToken);
+
+  return redirect("/");
+}
---
+++ 22-authentication-updated/06-logout/frontend/src/pages/Logout.js
@@ -0,0 +1,7 @@
+import { redirect } from "react-router-dom";
+import { removeAuthToken } from "../util/auth";
+
+export function action() {
+  removeAuthToken();
+  return redirect("/");
+}
