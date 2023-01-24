f261c565de8f24c58eea3fcbef5aad2da3b1232d
Nathan Workman <nathanjonworkman@gmail.com>
2023-01-23 20:25:00 -0600

    22-authentication-updated/09-finished

--- 22-authentication-updated/09-finished/frontend/src/pages/Root.js
+++ 22-authentication-updated/09-finished/frontend/src/pages/Root.js
@@ -1,10 +1,31 @@
-import { Outlet } from "react-router-dom";
-//import { Outlet, useNavigation } from 'react-router-dom';
-
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { getTokenDuration } from "../util/auth";
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const submit = useSubmit();
+  const authToken = useLoaderData();
+  useEffect(() => {
+    console.log("RootLayout::useEffect-start authToken=", authToken);
+    if (authToken === "EXPIRED") {
+      console.log("RootLayout::useEffect authToken EXPIRED");
+      submit(null, { action: "/logout", method: "post" });
+    } else if (authToken) {
+      console.log("RootLayout::useEffect-start calculateTokenDuration");
+      const tokenDuration = getTokenDuration();
+      console.log(tokenDuration);
+
+      setTimeout(() => {
+        console.log("AuthToken timer, logout");
+        submit(null, { action: "/logout", method: "post" });
+      }, tokenDuration);
+    } else {
+      // Do nothing
+      console.log("RootLayout::useEffect no token");
+    }
+    console.log("RootLayout::useEffect-end");
+  }, [authToken, submit]);
 
   return (
     <>
--- 22-authentication-updated/09-finished/frontend/src/util/auth.js
+++ 22-authentication-updated/09-finished/frontend/src/util/auth.js
@@ -1,20 +1,55 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  console.log("auth::getTokenDuration-start");
+  const expirationDateTxt = localStorage.getItem("authTokenExpiration");
+  const expirationDate = new Date(expirationDateTxt);
+
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  console.log("auth::getTokenDuration-start duration=", duration);
+  return duration;
+}
+
 export function getAuthToken() {
-  const authToken = localStorage.getItem("authToken");
+  console.log("auth::getAuthToken-start");
+
+  let authToken = localStorage.getItem("authToken");
+  if (authToken) {
+    const tokenDuration = getTokenDuration();
+    if (tokenDuration <= 0) {
+      authToken = "EXPIRED";
+    }
+  }
+  console.log("auth::getAuthToken-end authToken=", authToken);
   return authToken;
 }
 
 export function setAuthToken(authToken) {
+  console.log("auth::setAuthToken-start");
+  const prevAuthToken = getAuthToken();
   localStorage.setItem("authToken", authToken);
+
+  if (prevAuthToken !== authToken) {
+    console.log("auth::setAuthToken calculate token duration");
+    const expirationDate = new Date();
+    expirationDate.setHours(expirationDate.getHours() + 1);
+    const expirationDateTxt = expirationDate.toISOString();
+    localStorage.setItem("authTokenExpiration", expirationDateTxt);
+    console.log("auth::setAuthToken expirationDateTxt=", expirationDateTxt);
+  }
+  console.log("auth::setAuthToken-end");
 }
 
 export function removeAuthToken() {
+  console.log("auth::removeAuthToken-start");
   localStorage.removeItem("authToken");
+  localStorage.removeItem("authTokenExpiration");
+  console.log("auth::removeAuthToken-end");
 }
-
 export function checkAuthLoader() {
   const authToken = getAuthToken();
+
   if (!authToken) {
     return redirect("/auth");
   }
